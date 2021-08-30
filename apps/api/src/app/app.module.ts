9880c57625a380adec49a60edbb5e1c7ab635beb
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2021-08-30 11:02:12 +0000

    setting up graphql

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-
+import { GqlModuleOptions, GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { environment } from '../environments/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// move to environment
+const graphqlOptions: GqlModuleOptions = {
+  autoSchemaFile: join(process.cwd(), 'apps/api/schema.gql'),
+  debug: !environment.production,
+  playground: !environment.production,
+  sortSchema: true,
+};
+
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot(graphqlOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
