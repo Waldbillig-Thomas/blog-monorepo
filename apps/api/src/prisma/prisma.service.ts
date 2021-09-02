b11e54dc4769d4694ed3173fa223aab313e7c229
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2021-09-02 20:51:11 +0000

    creating prisma service

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {
+    this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', () => app.close());
+  }
+}
