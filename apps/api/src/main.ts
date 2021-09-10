ddc6844b2c602206cb1db4ced4a1ac4c76b9e2a8
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2021-09-10 19:15:37 +0000

    switching to fastify

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 4000;
+  const port: string = process.env.PORT || '4000';
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  app.setGlobalPrefix('api');
+
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/api');
-    Logger.log('Listening at http://localhost:' + port + '/graphql');
+    Logger.log(`Listening at http://localhost:${port}/api`);
+    Logger.log(`Listening at http://localhost:${port}/graphql`);
   });
 }
 
