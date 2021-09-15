a7dd87097f6a8f7a16104105e3a30eee1b674c7b
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2021-09-15 23:19:43 +0000

    fixup seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,18 @@
 import { Gender, Prisma, PrismaClient } from '@prisma/client';
+import { Chance } from 'chance';
+
+const chance = new Chance(1337);
 
 const prisma: PrismaClient = new PrismaClient();
 
 function getAuthor(index: number): Prisma.AuthorCreateInput {
+  const gender = chance.gender() as 'male' | 'female';
+
   return {
-    firstName: 'firstName ' + index,
-    lastName: 'lastName',
-    gender: Gender.FEMALE,
+    id: chance.guid(),
+    firstName: chance.first({ gender }),
+    lastName: chance.last(),
+    gender: { Female: Gender.FEMALE, Male: Gender.MALE }[gender],
     posts: {
       createMany: {
         data: Array.from({ length: 10 }).map((current, index) =>
@@ -22,8 +28,11 @@ function getPost(index: number): Prisma.PostCreateWithoutAuthorInput {
 }
 
 async function main() {
-  console.log(`Start seeding ...`);
+  console.log(`Clearing database ...`);
+  await prisma.post.deleteMany();
+  await prisma.author.deleteMany();
 
+  console.log(`Start seeding ...`);
   await Array.from({ length: 100 }).reduce(async (previous, current, index) => {
     await previous;
     return prisma.author.create({ data: getAuthor(index) });
