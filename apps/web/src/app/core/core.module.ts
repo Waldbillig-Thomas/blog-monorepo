6149d61cdf9ce80e1fc23682a717b859b97cde11
Thomas Waldbillig <waldbillig.thomas@gmail.com>
2021-09-17 21:09:13 +0000

    fixing button

--- apps/web/src/app/core/core.module.ts
+++ apps/web/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +11,7 @@ import { NavigationComponent } from './navigation.component';
   imports: [
     CommonModule,
     LayoutModule,
+    MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
